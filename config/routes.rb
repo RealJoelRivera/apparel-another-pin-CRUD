2d9e2b5614c53b48a499690301a928ae44ffd1da
Joel <realjoelrivera@gmail.com>
2020-04-07 15:20:59 -0400

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
+  get "info", to: "pages#info"
+
   resources :products
-  root "products#index"
+  root "pages#home"
 end
