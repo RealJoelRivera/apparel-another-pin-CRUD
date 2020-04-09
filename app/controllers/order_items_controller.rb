165f9eb3c0f751f5d13cf5895007e93dde3e4ceb
Joel <realjoelrivera@gmail.com>
2020-04-09 13:15:15 -0400

    Functionality to update the quantity of each item

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -3,6 +3,15 @@ class OrderItemsController < ApplicationController
     @product = Product.find(params[:product_id])
     @quantity = form_params[:quantity]
     @current_cart.order_items.create(product: @product, quantity: @quantity)
+    flash[:success] = "Thanks for adding to your cart"
+    redirect_to product_path(@product)
+  end
+
+  def update
+    @product = Product.find(params[:product_id])
+    @order_item = OrderItem.find(params[:id])
+    @order_item.find(form_params)
+    flash[:success] = "Thanks for updating to your cart"
     redirect_to product_path(@product)
   end
 
